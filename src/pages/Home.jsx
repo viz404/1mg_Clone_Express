b30a57e65096099bc566fb5e70d4a2c257f17507
vishal <vishal.gxm@gmail.com>
2023-02-20 17:36:00 +0530

    changed order or items

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,11 +6,6 @@ import SmallCarousal from "../components/home/SmallCarousal";
 const health_array = [
   {
     image:
-      "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/c2a0598f-483c-48ff-9783-71e402aa28d3.png",
-    title: "Diabetes Care",
-  },
-  {
-    image:
       "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/ab1da5f4-c074-47d2-b278-a5fbd2c93f1f.png",
     title: "Cardiac Care",
   },
@@ -39,6 +34,11 @@ const health_array = [
       "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/fa7ff8e9-f1f8-4a3e-9125-c9b36bcf38cd.png",
     title: "Pain Relief",
   },
+  {
+    image:
+      "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/c2a0598f-483c-48ff-9783-71e402aa28d3.png",
+    title: "Diabetes Care",
+  },
 ];
 
 const brand_array = [
@@ -46,74 +46,86 @@ const brand_array = [
     image:
       "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/fccafd09-569d-419d-b65a-84b6d314295a.png",
     title: "Cetaphil",
+    redirect: "/products?search=cetaphil",
   },
   {
     image:
       "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/e657a4c7-8cee-4eb6-850f-e48d1dd1288c.png",
     title: "Mamaearth",
+    redirect: "/products?search=mamaearth",
   },
   {
     image:
       "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/21c4a8bf-2e3a-42a1-83e9-6ef63e760f88.png",
     title: "Dabur",
+    redirect: "/products?search=dabur",
   },
   {
     image:
       "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/f28e82bd-37ec-4891-8eb7-69486635a199.png",
     title: "mCaffeine",
+    redirect: "/products?search=mcaffeine",
   },
   {
     image:
       "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/eba5fb6b-6baa-4bc0-b030-793645a2ee4e.png",
     title: "Nivea",
+    redirect: "/products?search=nivea",
   },
   {
     image:
       "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/f142c987-e7aa-48da-90b3-306cd90ea1c9.png",
     title: "Accu-Chek",
+    redirect: "/products?search=accu-chek",
   },
   {
     image:
       "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/1e9dae18-a318-405b-b97f-36c3c1e9b9e4.png",
     title: "Protinex",
+    redirect: "/products?search=protinex",
   },
 ];
 
 const popular_categories = [
   {
     image:
-      "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/b3fcf5ba-ae5e-4c94-a43f-e381cd27d5ab.webp",
-    title: "Top Deals",
+      "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/c2a0598f-483c-48ff-9783-71e402aa28d3.png",
+    title: "Diabetes",
+    redirect: "/products/diabetes",
   },
   {
-    image:
-      "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/ba7328d6-022b-45fe-b87a-1f1fbead2789.png",
-    title: "Winter Care",
-  },
-  {
-    image:
-      "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/f5fb69f7-cf7a-4e7c-ba48-b924c7b5111c.png",
-    title: "Nutritional Drinks",
+    image: "https://iili.io/HGaiwKJ.png",
+    title: "Healthcare Devices",
+    redirect: "/products/healthcare",
   },
   {
     image:
       "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/5adb8954-6264-46d7-8136-cb5a633b7535.png",
     title: "Ayurveda",
+    redirect: "/products/ayurveda",
   },
   {
     image:
-      "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/15e1ce5f-798f-4aa7-b6b4-1b140c4d6f4d.webp",
-    title: "Sexual Wellness",
+      "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/b57a9dd9-3b68-4f5f-801a-86c53113ea76.png",
+    title: "Personal Care",
+    redirect: "/products/personal",
   },
   {
     image:
       "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/668f0b65-56ef-4875-8aef-9a53b919b53c.webp",
     title: "Vitamins & Supplements",
+    redirect: "/products/vitamins",
   },
   {
     image:
-      "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/de1183ce-6b14-4549-9fda-f732d96a11de.webp",
-    title: "Skin Care",
+      "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/00c99add-c5ee-4254-8184-d43ef024c38e.png",
+    title: "Homeopathy",
+    redirect: "/products/homeopathy",
+  },
+  {
+    image:
+      "https://onemg.gumlet.io/a_ignore,w_150,h_150,c_fit,q_auto,f_auto/f5fb69f7-cf7a-4e7c-ba48-b924c7b5111c.png",
+    title: "Nutritional Drinks",
   },
 ];
 
@@ -164,22 +176,25 @@ const Home = () => {
       </div>
       <div className={styles.shopHealth}>
         <div className={styles.caption}>
-          <p>Shop by health concerns</p>
+          <p>Popular categories</p>
         </div>
-        <SmallCarousal data={health_array} />
+        <SmallCarousal data={popular_categories} />
       </div>
+
       <div className={styles.shopHealth}>
         <div className={styles.caption}>
           <p>Featured brands</p>
         </div>
         <SmallCarousal data={brand_array} />
       </div>
+
       <div className={styles.shopHealth}>
         <div className={styles.caption}>
-          <p>Popular categories</p>
+          <p>Shop by health concerns</p>
         </div>
-        <SmallCarousal data={popular_categories} />
+        <SmallCarousal data={health_array} />
       </div>
+
       <div className={styles.shopHealth}>
         <div className={styles.caption}>
           <p>Face Care Products</p>
